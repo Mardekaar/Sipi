0b7d8743d175cc957399c99755b2b803d0073797
Sipi <tibors11@gmail.com>
2024-07-03 11:17:43 +0200

    del files

--- GitHubBranchDelete/obj/Debug/netcoreapp3.0/GitHubBranchDelete.AssemblyInfo.cs
+++ GitHubBranchDelete/obj/Debug/netcoreapp3.0/GitHubBranchDelete.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubBranchDelete")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509e059bedfeb0d6af6d9b147be42fb4d0bcf8d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5f72d63f5f396b9220359c692d307d53a3c238")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubBranchDelete")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHubBranchDelete")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
