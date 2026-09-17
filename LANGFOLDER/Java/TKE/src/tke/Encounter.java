54bb070e5c60a8ecb52c5a14f836e8933ebb982f
Mardekaar <tibors11@gmail.com>
2026-09-17 12:21:15 +0200

    updating Java

--- LANGFOLDER/Java/TKE/src/tke/Encounter.java
+++ LANGFOLDER/Java/TKE/src/tke/Encounter.java
@@ -32,38 +32,38 @@ public class Encounter extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     @Override
     public void start(Stage stage) {
         Group root = new Group();
         Scene canvas = new Scene(root, 800, 540, Color.BLACK);
         stage.setScene(canvas);
-        
+
         Stop[] rgStops = {new Stop(.83, Color.WHITE), new Stop(1, Color.BLACK)};
         final RadialGradient rg = new RadialGradient(
             0, 0, 0, 0, 60, false, CycleMethod.NO_CYCLE, rgStops);
-        
+
         //cX, cY, radius, color
         final Circle finder = new Circle(400f, 270f, 50f, Color.WHITE);
         finder.setStroke(rg);
         finder.setStrokeWidth(10.);
         finder.setStrokeType(StrokeType.OUTSIDE);
-        
+
         Rectangle rogueFog = new Rectangle(0, 0, 800, 540);
         rogueFog.setFill(Color.BLACK);
         root.getChildren().add(rogueFog);
-        
+
         Image background = new Image("background.jpg", false);
         ImageView terrain = new ImageView();
         terrain.setImage(background);
         terrain.setClip(finder);
         terrain.setCursor(Cursor.CROSSHAIR); //no mouse cursor
         root.getChildren().add(terrain);
-        
+
         final GaussianBlur gb = new GaussianBlur();
         gb.setRadius(9);
         terrain.setEffect(gb);
-        
+
         //let the circle finder follow the mouse
         canvas.setOnMouseMoved(new EventHandler<MouseEvent>() {
             @Override public void handle(MouseEvent me) {
@@ -72,9 +72,9 @@ public class Encounter extends Application {
                 finder.setCenterY(me.getY());
             }
         });
-        
+
         //zoom-in/zoom-out with scrollwheel
-        canvas.setOnScroll(new EventHandler<ScrollEvent>() {            
+        canvas.setOnScroll(new EventHandler<ScrollEvent>() {
             @Override public void handle(ScrollEvent se) {
                 //System.out.println("x: "+ se.getDeltaX() +", y: "+ se.getDeltaY());
                 System.out.println("radius: "+ gb.getRadius());
@@ -87,7 +87,7 @@ public class Encounter extends Application {
                 }
             }
         });
-        
+
         stage.show();
     }
 }
